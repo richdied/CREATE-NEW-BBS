98e69f3fc00f66ce9e4e322b75fc1dbcf434050b
ryanj <ryanj@DESKTOP-8PVB0UI>
2020-12-21 19:22:17 +0900

    JSP Ajax 고급 회원제 채팅 사이트 개발하기 회원가입 DB 기능 구현

---
+++ UserChat/src/user/UserDAO.java
@@ -0,0 +1,120 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class UserDAO {
+	DataSource dataSource;
+	
+	public UserDAO() {
+		try {
+			InitialContext initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			dataSource = (DataSource) envContext.lookup("jdbc/UserChat");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int login(String userID, String userPassword) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "SELECT * FROM USER WHERE userID = ?";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1,  userID);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				if(rs.getString("userPassword").equals(userPassword)) {
+					return 1;
+				}
+				return 2;
+ 			} else {
+ 				return 0;
+ 			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+		}
+	}
+      return -1;
+	}		
+	
+	public int registerCheck(String userID) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "SELECT * FROM USER WHERE userID = ?";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1,  userID);
+			rs = pstmt.executeQuery();
+			if (rs.next() || userID.equals("")) {
+				return 0;
+ 			}else {
+ 				return 1;
+ 			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+		}
+	}
+      return -1;
+	}		
+	public int register(String userID, String userPassword, String userName, String userAge, String userGender, String userEmail, String userProfile) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "INSERT INTO USER VALUES(?, ?, ?, ?, ?, ?, ?)";;
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1,  userID);
+			pstmt.setString(2, userPassword);
+			pstmt.setString(3, userName);
+		    pstmt.setInt(4, Integer.parseInt(userAge));
+			pstmt.setString(5, userGender);
+			pstmt.setString(6, userEmail);
+			pstmt.setString(7, userProfile);
+			rs = pstmt.executeQuery();
+			if (rs.next() || userID.equals("")) {
+				return 0;
+ 			}else {
+ 				return 1;
+ 			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+		}
+	}
+      return -1;
+	}		
+	
+	
+}
