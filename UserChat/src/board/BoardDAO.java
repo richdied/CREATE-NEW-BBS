faaf4307c83cccb0b6ed896e914f0d5a21de9cfa
ryanj <ryanj@DESKTOP-8PVB0UI>
2021-01-12 18:37:29 +0900

    JSP Ajax 고급 회원제 채팅 사이트 개발하기 파일 및 게시글 등록 기능 구현

---
+++ UserChat/src/board/BoardDAO.java
@@ -0,0 +1,88 @@
+package board;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class BoardDAO {
+	
+ DataSource dataSource;
+	
+	public BoardDAO() {
+		try {
+			InitialContext initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			dataSource = (DataSource) envContext.lookup("jdbc/UserChat");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int write(String userID, String boardTitle, String boardContent, String boardFile, String boardRealFile) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String SQL = "INSERT INTO BOARD SELECT ?, IFNULL((SELECT MAX(boardID) + 1 FROM BOARD), 1), ?, ?, now(), 0, ?, ?, IFNULL((SELECT MAX(boardGroup) + 1 FROM BOARD), 0), 0, 0";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1,  userID);
+			pstmt.setString(2, boardTitle);
+			pstmt.setString(3, boardContent);
+		    pstmt.setString(4, boardFile);
+		    pstmt.setString(5, boardRealFile);
+	        return pstmt.executeUpdate();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+			return -1; 
+		}		
+	
+	public BoardDTO getBoard(String boardID) {
+		BoardDTO board = new BoardDTO();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "SELECT * FROM BOARD WHERE boardID = ?";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1,  boardID);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				board.setUserID(rs.getString("userID"));
+				board.setBoardID(rs.getInt("boardID"));
+				board.setBoardTitle(rs.getString("boardTitle").replaceAll(" ", "&nbsp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br>"));
+				board.setBoardContent(rs.getString("boardContent").replaceAll(" ", "&nbsp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br>"));
+				board.setBoardDate(rs.getString("boardDate").substring(0, 11));
+				board.setBoardHit(rs.getInt("boardHit"));
+				board.setBoardFile(rs.getString("boardFile"));
+				board.setBoardRealFile(rs.getString("boardRealFile"));
+				board.setBoardGroup(rs.getInt("boardGroup"));
+				board.setBoardSequence(rs.getInt("boardSequence"));
+				board.setBoardLevel(rs.getInt("boardLevel"));
+ 			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+		}
+	}
+      return board;
+	}		
+}
